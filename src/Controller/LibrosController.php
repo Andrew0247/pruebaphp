7d7fa3bc28a0d0dff8d1328be53eb888d02797b2
Andrew0247 <62215737+Andrew0247@users.noreply.github.com>
2021-05-11 17:58:48 -0500

    Creacion repositorio

---
+++ src/Controller/LibrosController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\HttpFoundation\Request;
+
+class LibrosController extends AbstractController
+{
+    /**
+     * @Route("/libros", name="libros")
+     */
+    public function index(Request $request): Response
+    {
+        $client = HttpClient::create();
+        $autor = $request->get('autor');
+        $año = $request->get('año');
+        $lenguaje = $request->get('lenguaje');
+        print_r($autor);
+
+        if($autor != '' or $año != '' or $lenguaje != ''){
+
+            $url = "https://www.etnassoft.com/api/v1/get/?category=libros_programacion&book_author=$autor&publisher_date=$año&any_tags=[$lenguaje]";
+            $content = $client->request('GET', $url);
+            $cuerpo = $content->getContent();
+            $decodificacion = json_decode($cuerpo);
+
+        }else{
+
+            $content = $client->request('GET', 'https://www.etnassoft.com/api/v1/get/?category=libros_programacion');
+            $cuerpo = $content->getContent();
+            $decodificacion = json_decode($cuerpo);
+            // print_r($decodificacion);
+        
+        }
+            
+        return $this->render('libros/index.html.twig', [
+            'controller_name' => 'LibrosController',
+           'contenido' => $decodificacion,
+        ]);
+    }
+
+    /**
+     * @Route("/verLibro/{id}", name="ver_libro")
+     */
+    public function ver(string $id): Response
+    {
+        $client = HttpClient::create();
+        $url = "https://www.etnassoft.com/api/v1/get/?id=$id";
+        $content = $client->request('GET', $url);
+        $cuerpo = $content->getContent();
+        $decodificacion = json_decode($cuerpo);
+        // print_r($decodificacion);
+
+        return $this->render('libros/ver_libro.html.twig', [
+            'controller_name' => 'LibrosController',
+           'contenido' => $decodificacion,
+        ]);
+    }
+
+    /**
+     * @Route("/filtros", name="filtros")
+     */
+    public function filtros(): Response
+    {
+
+        return $this->render('libros/filtros.html.twig', [
+            'controller_name' => 'LibrosController',
+        ]);
+    }
+}
